cf40c96ae1afc88a28791d19db0f36c68c346ce4
Lion Pierau <lion.pierau@gmail.com>
2017-11-08 12:18:12 +0100

    added class for a simpler connection

---
+++ src/main/java/vsp/RESTController.java
@@ -0,0 +1,25 @@
+package vsp;
+
+
+import vsp.utility.GETRequest;
+
+import java.io.IOException;
+
+public class RESTController {
+
+    private static final String IP = "172.19.0.3";
+    private static final String PORT = "5000";
+    private static final String URL_STR = "http://" + IP + ":" + PORT;
+
+    public static void main(String[] args) {
+        try {
+
+            System.out.println("Output from Server .... \n");
+            GETRequest request = new GETRequest(URL_STR);
+
+            System.out.println(request.getResponse());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
